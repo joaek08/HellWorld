0a8b430be42a036e6cd34309ae0af65039b15ba0
joaek08 <joakim.ekstrom@liu.se>
2013-08-16 17:31:46 +0200

    now with git

--- mainproject/HellWorld/src/HellWorld.java
+++ mainproject/HellWorld/src/HellWorld.java
@@ -3,7 +3,7 @@ public class HellWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("World is Hell some days");
+System.out.println("World is Hell some days, but now Git almost work");
 	}
 
 }
